437fb2d6f87d18ac58fbea635ad8fe2dd7ee013c
mishchuk <mishchuksg@gmail.com>
2025-01-17 13:59:25 +0300

    Написаны функции

---
+++ src/external_api.py
@@ -0,0 +1,25 @@
+import os
+from http.client import responses
+
+import requests
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+API_KEY = os.getenv("API_KEY")
+
+def convert_current(valuta, rub, amount) -> float:
+    """ Функция обращается к внешнему API и производит конвертацию валюты. """
+    response = requests.get(
+        f"https://api.apilayer.com/exchangerates_data/convert?to={rub}&from={valuta}&amount={amount}&apikey={API_KEY}")
+
+    if response.status_code == 200:
+        currency_rate = response.json()['info']['rate']#Определяем текущий курс
+        result = response.json()['result']
+        print(f"\nБыла произведена конвертация валюты из {valuta} в {rub} по курсу: {currency_rate}")
+        return result
+    else:
+        print("\nЧто-то пошло не так с запросом на конвертацию валюты.")
+
+
+
+#print(convert_current("USD", "RUB", 1))
---
+++ src/utils.py
@@ -0,0 +1,36 @@
+import json
+from external_api import convert_current
+
+
+def convert_transactions(path: str) -> list:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей"""
+    try:
+        with open(path, encoding="utf-8") as transactions_file:
+            try:
+                transactions = json.load(transactions_file)
+            except json.JSONDecodeError:
+                print("Ошибка декодирования файла")
+                return []
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
+
+    return transactions
+
+
+
+def transaction_amount(transaction: dict) -> float:
+    """Функция возвращает сумму транзакции в рублях.
+    Если транзакция была в USD или EUR, происходит обращение к внешнему API
+    для получения текущего курса валют и конвертации суммы операции в рубли."""
+    valuta = transaction["operationAmount"]["currency"]["code"]#Определяем код валюты
+    amount = transaction["operationAmount"]["amount"]#Определяем сумму валюты
+    if valuta == "RUB":
+        return amount
+    else:
+        result = convert_current(valuta, "RUB", amount)
+        return result
+
+
+
+
